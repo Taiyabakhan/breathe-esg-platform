f93a92b49e4eea950221d5e857fcc8d4828e11ed
Taiyaba Khan <146852439+Taiyabakhan@users.noreply.github.com>
2026-05-26 21:00:23 +0530

    Update apps.py

--- backend/ingestion/apps.py
+++ backend/ingestion/apps.py
@@ -4,12 +4,12 @@ class IngestionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ingestion'
 
-    def ready(self):
-        from django.contrib.auth.models import User
+    # def ready(self):
+    #     from django.contrib.auth.models import User
 
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                'admin',
-                'admin@gmail.com',
-                'admin123'
-            )
+    #     if not User.objects.filter(username='admin').exists():
+    #         User.objects.create_superuser(
+    #             'admin',
+    #             'admin@gmail.com',
+    #             'admin123'
+    #         )
